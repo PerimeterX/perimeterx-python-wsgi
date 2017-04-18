dcf4f4648ae7aa6962a2f88b5f7d19135b7c3153
bendpx <ben@perimeterx.com>
2017-04-18 12:31:39 +0300

    addec changlog file + updated version

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/)
+and this project adheres to [Semantic Versioning](http://semver.org/).
+
+## [1.2.0] - 2017-04-18
+### Added
+- Added UUID to page requested
+- New block/captcha templates
+- Delete captcha cookie after evaluation
+- Sending original cookie value when decryption fails
--- setup.py
+++ setup.py
@@ -2,15 +2,16 @@
 
 from setuptools import setup
 
+version = 'v1.2.0'
 setup(name='perimeterx-python-wsgi',
-      version='v1.0.3',
+      version=version,
       description='PerimeterX WSGI middleware',
       author='Ben Diamant',
       author_email='ben@perimeterx.com',
       url='https://github.com/PerimeterX/perimeterx-python-wsgi',
-      download_url='https://github.com/PerimeterX/perimeterx-python-wsgi/tarball/v1.0.2',
+      download_url='https://github.com/PerimeterX/perimeterx-python-wsgi/tarball/' + version,
       package_dir={'perimeterx': 'perimeterx'},
       install_requires=[
-        "pystache"
+          "pystache"
       ]
       )
