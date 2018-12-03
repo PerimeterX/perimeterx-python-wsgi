760e71b52fb6dad33d6ed8908356cd7510ca7421
Alex Bluvstein <alex.bluvstein@perimeterx.com>
2018-12-03 15:37:47 +0200

    Fixed version declartion in setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,16 @@
 
 from setuptools import setup
 
+version = 'v2.0.0'
 setup(name='perimeterx-python-wsgi',
       packages='perimeterx-python-wsgi',
-      version='v2.0.0',
+      version=version,
       license='MIT',
       description='PerimeterX WSGI middleware',
       author='Ben Diamant',
       author_email='ben@perimeterx.com',
       url='https://github.com/PerimeterX/perimeterx-python-wsgi',
-      download_url='https://github.com/PerimeterX/perimeterx-python-wsgi/tarball/v2.0.0',
+      download_url='https://github.com/PerimeterX/perimeterx-python-wsgi/tarball/' + version,
       package_dir={'perimeterx': 'perimeterx'},
       install_requires=[
         "pystache==0.5.4", 'requests==2.20.1', 'setuptools==40.6.2', 'requests_mock==1.5.2',
