e5d6bba65530d7e69320eaad47662bfc9792bcfd
Alex Bluvstein <alex.bluvstein@perimeterx.com>
2018-12-05 14:00:22 +0200

    Cahnged dependecies

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 
 from setuptools import setup, find_packages
 
+install_requires = open('./requirements.txt', 'r').readlines()
 version = 'v2.0.1'
 setup(name='perimeterx-python-wsgi',
       version=version,
@@ -13,7 +14,6 @@ setup(name='perimeterx-python-wsgi',
       download_url='https://github.com/PerimeterX/perimeterx-python-wsgi/tarball/' + version,
       packages=find_packages(exclude=['dev', 'test*']),
       package_data={'perimeterx': ['templates/*']},
-      install_requires=[
-          "pystache==0.5.4", 'requests==2.20.1', 'setuptools==40.6.2', 'Crypto'],
+      install_requires=install_requires,
       classifiers=['Intended Audience :: Developers',
                    'Programming Language :: Python :: 2.7'])
