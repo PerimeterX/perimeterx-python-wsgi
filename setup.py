b31e6a3b6b8b25535b93e958eb8bfc75884aecb1
alexbpx <41577203+alexbpx@users.noreply.github.com>
2019-01-13 14:55:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,6 @@ setup(name='perimeterx-python-wsgi',
       download_url='https://github.com/PerimeterX/perimeterx-python-wsgi/tarball/' + version,
       packages=find_packages(exclude=['dev', 'test*']),
       package_data={'perimeterx': ['templates/*']},
-      install_requires=['pystache>=0.5.1,<=0.5.4', 'requests>=2.18.4,<=2.20.1', 'setuptools==40.6.2', 'Werkzeug==0.14.1', 'pycryptodome==3.7.2'],
+      install_requires=['pystache>=0.5.1,<=0.5.4', 'requests>=2.18.4,<=2.20.1', 'setuptools==40.6.2', 'Werkzeug==0.14.1', 'pycryptodome>=3.7.2, <4.0.0'],
       classifiers=['Intended Audience :: Developers',
                    'Programming Language :: Python :: 2.7'])
