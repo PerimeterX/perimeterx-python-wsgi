c6cd238fd09ab14d1c5af71b168adc9bcddb2e94
pxjohnny <johnny@perimeterx.com>
2019-02-24 22:45:57 +0200

    px_logger refactor

--- perimeterx/px_logger.py
+++ perimeterx/px_logger.py
@@ -1,11 +1,21 @@
+import logging
+
 class Logger(object):
     def __init__(self, debug, app_id):
         self.debug_mode = debug
         self.app_id = app_id
 
+        # Setup logger
+        self.logger = logging.getLogger(__name__)
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter('[PerimeterX %(levelname)s][{}]: %(message)s'.format(self.app_id))
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+        self.logger.setLevel(logging.DEBUG)
+
     def debug(self, message):
-        if self.debug_mode:
-            print '[PerimeterX DEBUG][{}]: '.format(self.app_id) + message
+		if self.debug_mode:
+			self.logger.debug(message)
 
     def error(self, message):
-        print '[PerimeterX ERROR][{}]: '.format(self.app_id) + message
+        self.logger.error(message)
