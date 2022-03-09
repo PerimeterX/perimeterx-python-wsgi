b551e7b30691556daf727eda12ca0e701e4e9b6f
ori-gold-px <77228321+ori-gold-px@users.noreply.github.com>
2022-03-09 18:13:06 +0200

    Revert "[SDKNEW-1655] Feature/new block page"

--- test/test_px_blocker.py
+++ test/test_px_blocker.py
@@ -103,13 +103,14 @@ class Test_PXBlocker(unittest.TestCase):
                             'vid': 'bf619be8-94be-458a-b6b1-ee81f154c282',
                             'jsRef': '',
                             'hostUrl': '/fake_app_id/xhr',
-                            'customLogo': '',
+                            'customLogo': None,
                             'appId': 'PXfake_app_id',
                             'uuid': '8712cef7-bcfa-4bb6-ae99-868025e1908a',
+                            'logoVisibility': 'hidden',
                             'jsClientSrc': '/fake_app_id/init.js',
                             'firstPartyEnabled': 'true',
-                            'cssRef': '',
-                            'altBlockScript': '//captcha.px-cloud.net/PXfake_app_id/captcha.js?a=&u=8712cef7-bcfa-4bb6-ae99-868025e1908a&v=bf619be8-94be-458a-b6b1-ee81f154c282&m=0'}
+                            'refId': '8712cef7-bcfa-4bb6-ae99-868025e1908a',
+                            'cssRef': ''}
         self.assertDictEqual(message, expected_message)
         expected_message['blockScript'] = '/fake_app/captcha/captcha.js?a=&u=8712cef7-bcfa-4bb6-ae99-868025e1908a&v=bf619be8-94be-458a-b6b1-ee81f154c282&m=0'
         self.assertNotEqual(message, expected_message)
